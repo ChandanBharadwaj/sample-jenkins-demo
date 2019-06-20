78cf3b0bcdd35a96368815eed9c986cc3062cc35
Chandan Bharadwaj <kolluruchandanbharadwaj@gmail.com>
2019-06-20 18:00:04 +0530

    Update sample.groovy

--- sample.groovy
+++ sample.groovy
@@ -1,5 +1,11 @@
 def runJenkinsPipeline(buildScript) {
  println "Sample Groovy Script testing "
+  stage("upload") {
+        def inputFile = input message: 'Upload file', parameters: [file(name: 'C:\Users\vinutha.g\Desktop\application.properties')]
+        new hudson.FilePath(new File("$workspace/application.properties")).copyFrom(inputFile)
+        inputFile.delete()
+       println "File uploaded succesfully"
+   }
  stage('File Param WA') {
           writeFile file: 'demo.yaml', text: params.INPUT_PARAM
           println params.INPUT_PARAM
