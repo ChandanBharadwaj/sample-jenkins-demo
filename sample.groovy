1e7d54bfe624b583bf33617f9429292a020a9e3c
Chandan Bharadwaj <kolluruchandanbharadwaj@gmail.com>
2019-06-20 17:52:33 +0530

    Update sample.groovy

--- sample.groovy
+++ sample.groovy
@@ -1,3 +1,7 @@
 def runJenkinsPipeline(buildScript) {
  println "Sample Groovy Script testing "
+ stage('File Param WA') {
+          writeFile file: 'demo.yaml', text: params.INPUT_PARAM
+          println params.INPUT_PARAM
+}
 }
